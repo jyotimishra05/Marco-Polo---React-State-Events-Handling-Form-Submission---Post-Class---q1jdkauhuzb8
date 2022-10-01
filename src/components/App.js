761bf731a2c1d04b9002a247f081be27f2b0dd17
galaxy28 <51984459+jyotimishra05@users.noreply.github.com>
2022-10-01 15:45:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+  const headingTag="Marco";
+  const btn ="Polo";
+  const[heading, setHeading]=useState(headingTag)
+  const[insideButton, setInsideButton]=useState(btn)
+  // const [name,setName]=useState({firstname:"",lastname:""})
+  
+  const handleClick=()=>{
+    if(heading === "Marco" && insideButton==="Polo"){
+      setHeading("Polo");
+      setInsideButton("Marco")
+    }
+    else{
+      setHeading("Marco");
+      setInsideButton("Polo")
+
+    }
+    
+ 
+    
+
+  }
   
   return (
     <div id="main">
+    <h1 id="marco-polo">{heading}</h1>
+  <button id="marco-polo-toggler" onClick={handleClick}>{insideButton}</button>
 
     </div>
   )
